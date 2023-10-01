fd34c246a6146cce674a23ef6c0b3c347182c64a
Max Woollons <maxwoollons@outlook.com>
2023-10-01 20:41:15 +1000

    update code to allow graph in gui

--- ApplicationFiles/main.py
+++ ApplicationFiles/main.py
@@ -6,6 +6,9 @@ from tkinter import messagebox
 import numpy as np
 from tkinter import ttk
 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
 # Data years we have available
 options = [
     "2013",
@@ -32,7 +35,8 @@ vicholidays = ['1-1','2-1','26-1','13-3','7-4','8-4','9-4','10-4','25-4','12-6',
 
 root = tk.Tk()
 root.title("Data Visualization Project - Victoria Crash Data")
-root.geometry("800x500")
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry("%dx%d+0+0" % (w, h))
 root.iconbitmap("logo.ico")
 
 style = ttk.Style()
@@ -68,6 +72,7 @@ drop2.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
 
 
 
+
 # Second Option
 
 
@@ -79,7 +84,6 @@ framebtn2.columnconfigure(0, weight=1)
 framebtn2.columnconfigure(1, weight=1)
 framebtn2.columnconfigure(2, weight=1)
 framebtn2.pack()
-
 button2 = ttk.Button(framebtn2, text="Show Graph", command=lambda: show_graph2())
 button2.grid(row=0, column=2, sticky="ew", padx=5, pady=5)
 entry3 = tk.StringVar()
@@ -131,14 +135,23 @@ graph5.pack()
 framebtn5 = tk.Frame(root)
 framebtn5.columnconfigure(0, weight=1)
 framebtn5.pack()
-
 button7 = ttk.Button(framebtn5, text="Types of Accidents", command=lambda: show_graph5())
 button7.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 button8 = ttk.Button(framebtn5, text="Alcohol Hourly Distrubution", command=lambda: show_graph52())
 button8.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
 
+
+
+# Bottom of the page for graphs
+bottomframe = tk.Frame(root)
+bottomframe.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+bottomframe.grid_rowconfigure(0, weight=1)
+bottomframe.grid_columnconfigure(0, weight=1)
+
+
 def show_graph1():
     # For a user-selected period, display the information of all accidents that happened in the period.
+    # GUI Intergration complete
     date1 = entry1.get()
     date2 = entry2.get()
     crash_data = pd.read_csv('data.csv')
@@ -153,26 +166,33 @@ def show_graph1():
         messagebox.showinfo("Error", "Start date cannot be greater than end date")
     elif date1 == date2:
         crash_data = crash_data[crash_data['YEAR'] == int(date1)]
-        plt.figure(figsize=(10,5))
+        fig, ax = plt.subplots(figsize=(10, 5))
         crash_data['ACCIDENT_TYPE'].value_counts().plot(kind='pie',autopct='%1.1f%%', labels=None)
-        plt.title('Types of Accidents between ' + date1 + ' and ' + date2)
-        plt.ylabel('')
-        plt.legend(loc='upper left', bbox_to_anchor=(-0.6, 0.6), labels=crash_data['ACCIDENT_TYPE'].unique())
-        plt.tight_layout()
-        plt.show()
-    else:
+        ax.set_title('Types of Accidents between ' + date1 + ' and ' + date2)
+        ax.set_ylabel('')
+        ax.legend(loc='upper left', bbox_to_anchor=(-0.6, 0.6), labels=crash_data['ACCIDENT_TYPE'].unique())
+        if hasattr(bottomframe, 'chart_type'):
+            bottomframe.chart_type.get_tk_widget().destroy()
+        chart_type = FigureCanvasTkAgg(fig, bottomframe)
+        chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        bottomframe.chart_type = chart_type
 
+
+    else:
         crash_data = crash_data[(crash_data['YEAR'] >= int(date1)) & (crash_data['YEAR'] <= int(date2))]    
-        plt.figure(figsize=(10,5))
-        
+        fig, ax = plt.subplots(figsize=(10, 5))
         crash_data['ACCIDENT_TYPE'].value_counts().plot(kind='pie',autopct='%1.1f%%', labels=None)
-        plt.title('Types of Accidents between ' + date1 + ' and ' + date2)
-        plt.ylabel('')
-        plt.legend(loc='upper left', bbox_to_anchor=(-0.6, 0.6), labels=crash_data['ACCIDENT_TYPE'].unique())
-        plt.tight_layout()
-        plt.show()      
+        ax.set_title('Types of Accidents between ' + date1 + ' and ' + date2)
+        ax.set_ylabel('')
+        ax.legend(loc='upper left', bbox_to_anchor=(-0.6, 0.6), labels=crash_data['ACCIDENT_TYPE'].unique())
+        if hasattr(bottomframe, 'chart_type'):
+            bottomframe.chart_type.get_tk_widget().destroy()
+        chart_type = FigureCanvasTkAgg(fig, bottomframe)
+        chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        bottomframe.chart_type = chart_type
 
 def show_graph2():
+    # GUI Intergration complete
     date1 = entry3.get()
     date2 = entry4.get()
     # For a user-selected period, produce a chart to show the number of accidents in each hour of the day (on average).
@@ -189,29 +209,38 @@ def show_graph2():
         crash_data = crash_data[crash_data['YEAR'] == int(date1)]
         crash_data["HOUR"] = crash_data['ACCIDENT_TIME'].str[:2]
         crash_data["HOUR"] = crash_data["HOUR"].astype('int32')
-        plt.figure(figsize=(10,5))
+        fig, ax = plt.subplots(figsize=(10, 5))
         plt.bar(crash_data['HOUR'].value_counts().index, crash_data['HOUR'].value_counts().values)
-        plt.title('Number of Accidents in each hour of the day between ' + date1 + ' and ' + date2)
-        plt.ylabel('Number of Accidents')
-        plt.xlabel('Hour of the day')
-        plt.xticks(rotation=0, ticks=crash_data['HOUR'].value_counts().index)
-        plt.tight_layout()
-        plt.show()
+        ax.set_title('Number of Accidents in each hour of the day between ' + date1 + ' and ' + date2)
+        ax.set_ylabel('Number of Accidents')
+        ax.set_xlabel('Hour of the day')
+        ax.set_xticks(crash_data['HOUR'].value_counts().sort_index().index)
+        ax.set_xticklabels(crash_data['HOUR'].value_counts().sort_index().index, rotation=45)
+        if hasattr(bottomframe, 'chart_type'):
+            bottomframe.chart_type.get_tk_widget().destroy()
+        chart_type = FigureCanvasTkAgg(fig, bottomframe)
+        chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        bottomframe.chart_type = chart_type
         
     else:
         crash_data = crash_data[(crash_data['YEAR'] >= int(date1)) & (crash_data['YEAR'] <= int(date2))]   
         crash_data["HOUR"] = crash_data['ACCIDENT_TIME'].str[:2]
         crash_data["HOUR"] = crash_data["HOUR"].astype('int32')
-        plt.figure(figsize=(10,5))
+        fig, ax = plt.subplots(figsize=(10, 5))
         plt.bar(crash_data['HOUR'].value_counts().index, crash_data['HOUR'].value_counts().values)
-        plt.title('Number of Accidents in each hour of the day between ' + date1 + ' and ' + date2)
-        plt.ylabel('Number of Accidents')
-        plt.xlabel('Hour of the day')
-        plt.xticks(rotation=0, ticks=crash_data['HOUR'].value_counts().index)
-        plt.tight_layout()
-        plt.show()
+        ax.set_title('Number of Accidents in each hour of the day between ' + date1 + ' and ' + date2)
+        ax.set_ylabel('Number of Accidents')
+        ax.set_xlabel('Hour of the day')
+        ax.set_xticks(crash_data['HOUR'].value_counts().sort_index().index)
+        ax.set_xticklabels(crash_data['HOUR'].value_counts().sort_index().index, rotation=45)
+        if hasattr(bottomframe, 'chart_type'):
+            bottomframe.chart_type.get_tk_widget().destroy()
+        chart_type = FigureCanvasTkAgg(fig, bottomframe)
+        chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        bottomframe.chart_type = chart_type
 
 def show_graph3():
+    # GUI Intergration complete
     crash_data = pd.read_csv('data.csv')
     crash_data['ACCIDENT_DATE'] = pd.to_datetime(crash_data['ACCIDENT_DATE'], format='%d/%m/%Y')
     crash_data['MONTH'] = crash_data['ACCIDENT_DATE'].dt.month
@@ -223,21 +252,24 @@ def show_graph3():
     keyword = entry5.get()
     if date1 == "Select Year":
         print("Error")
-        messagebox.showinfo("Error", "Start date cannot be greater than end date")
+        messagebox.showinfo("Error", "Please Select a year")
     elif keyword == "Keyword":
         print("Error")
         messagebox.showinfo("Error", "Please select a keyword")
     else:
         crash_data = crash_data[crash_data['YEAR'] == int(date1)]
         crash_data = crash_data[crash_data['ACCIDENT_TYPE'].str.contains(keyword)]
-        plt.figure(figsize=(10,5))
+        fig, ax = plt.subplots(figsize=(10, 5))
         crash_data['MONTH'].value_counts().sort_index().plot(kind='bar')
-        plt.title('Number of Accidents caused by ' + keyword + ' in ' + date1)
-        plt.ylabel('Number of Accidents')
-        plt.xlabel('Month')
-        plt.xticks(rotation=0, ticks=crash_data['MONTH'].value_counts().sort_index().index)
-        plt.tight_layout()
-        plt.show()
+        ax.set_title('Number of Accidents caused by ' + keyword + ' in ' + date1)
+        ax.set_ylabel('Number of Accidents')
+        ax.set_xlabel('Month')
+        ax.set_xticks(ticks=crash_data['MONTH'].value_counts().sort_index().index)
+        if hasattr(bottomframe, 'chart_type'):
+            bottomframe.chart_type.get_tk_widget().destroy()
+        chart_type = FigureCanvasTkAgg(fig, bottomframe)
+        chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        bottomframe.chart_type = chart_type
 
 
 # 4.1
@@ -247,7 +279,7 @@ def show_graph41():
     crash_data['YEAR'] = crash_data['ACCIDENT_DATE'].dt.year
     crash_data['ALCOHOL_RELATED'] = crash_data['ALCOHOL_RELATED'].str.lower()
     grouped_data = crash_data.groupby(['YEAR', 'ALCOHOL_RELATED'])['FATALITY'].mean().unstack(fill_value=0)
-    plt.figure(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(10, 5))
     num_years = len(grouped_data)
     bar_width = 0.35
     indices = np.arange(num_years)
@@ -265,13 +297,16 @@ def show_graph41():
         width=bar_width,
         alpha=0.7
     )
-    plt.xticks(indices, grouped_data.index)
-    plt.title('Average Fatalities in Alcohol vs. Non-Alcohol Related Accidents between 2013 and 2019')
-    plt.ylabel('Average Fatalities')
-    plt.xlabel('Year')
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+    ax.set_xticks(indices, grouped_data.index)
+    ax.set_title('Average Fatalities in Alcohol vs. Non-Alcohol Related Accidents between 2013 and 2019')
+    ax.set_ylabel('Average Fatalities')
+    ax.set_xlabel('Year')
+    ax.legend()
+    if hasattr(bottomframe, 'chart_type'):
+        bottomframe.chart_type.get_tk_widget().destroy()
+    chart_type = FigureCanvasTkAgg(fig, bottomframe)
+    chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    bottomframe.chart_type = chart_type
 
 # 4.2
 def show_graph42():
@@ -286,21 +321,20 @@ def show_graph42():
     non_alcohol_related = crash_data[crash_data['ALCOHOL_RELATED'] == 'no']
     sorted_alcohol_related = alcohol_related['HOUR'].value_counts().sort_index()
     sorted_non_alcohol_related = non_alcohol_related['HOUR'].value_counts().sort_index()
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.bar(sorted_alcohol_related.index, sorted_alcohol_related.values)
-    plt.title('Number of Alcohol-Related Accidents 2013-2019')
-    plt.ylabel('Number of Accidents')
-    plt.xlabel('Hour of the day')
-    plt.xticks(rotation=0)
-    plt.subplot(1, 2, 2)
-    plt.bar(sorted_non_alcohol_related.index, sorted_non_alcohol_related.values)
-    plt.title('Number of Non-Alcohol-Related Accidents 2013-2019')
-    plt.ylabel('Number of Accidents')
-    plt.xlabel('Hour of the day')
-    plt.xticks(rotation=0)
-    plt.tight_layout()
-    plt.show()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    ax1.bar(sorted_alcohol_related.index, sorted_alcohol_related.values)
+    ax1.set_title('Number of Alcohol-Related Accidents 2013-2019')
+    ax1.set_ylabel('Number of Accidents')
+    ax1.set_xlabel('Hour of the day')
+    ax2.bar(sorted_non_alcohol_related.index, sorted_non_alcohol_related.values)
+    ax2.set_title('Number of Non-Alcohol-Related Accidents 2013-2019')
+    ax2.set_ylabel('Number of Accidents')
+    ax2.set_xlabel('Hour of the day')
+    if hasattr(bottomframe, 'chart_type'):
+        bottomframe.chart_type.get_tk_widget().destroy()
+    chart_type = FigureCanvasTkAgg(fig, bottomframe)
+    chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    bottomframe.chart_type = chart_type
 
 # 4.3
 def show_graph43():
@@ -318,21 +352,22 @@ def show_graph43():
     custom_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     sorted_alcohol_related = alcohol_related['DAY_OF_WEEK'].value_counts().reindex(custom_order)
     sorted_non_alcohol_related = non_alcohol_related['DAY_OF_WEEK'].value_counts().reindex(custom_order)
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    sns.barplot(x=sorted_alcohol_related.index, y=sorted_alcohol_related.values, order=custom_order)
-    plt.title('Number of Alcohol-Related Accidents (All Data)')
-    plt.ylabel('Number of Accidents')
-    plt.xlabel('Day of the week')
-    plt.xticks(rotation=45)
-    plt.subplot(1, 2, 2)
-    sns.barplot(x=sorted_non_alcohol_related.index, y=sorted_non_alcohol_related.values, order=custom_order)
-    plt.title('Number of Non-Alcohol-Related Accidents (All Data)')
-    plt.ylabel('Number of Accidents')
-    plt.xlabel('Day of the week')
-    plt.xticks(rotation=45)  
-    plt.tight_layout()
-    plt.show()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    sns.barplot(x=sorted_alcohol_related.index, y=sorted_alcohol_related.values, order=custom_order, ax=ax1)
+    ax1.set_title('Number of Alcohol-Related Accidents (All Data)')
+    ax1.set_ylabel('Number of Accidents')
+    ax1.set_xlabel('Day of the week')
+    ax1.set_xticklabels(ax1.get_xticklabels())
+    sns.barplot(x=sorted_non_alcohol_related.index, y=sorted_non_alcohol_related.values, order=custom_order, ax=ax2)
+    ax2.set_title('Number of Non-Alcohol-Related Accidents (All Data)')
+    ax2.set_ylabel('Number of Accidents')
+    ax2.set_xlabel('Day of the week')
+    ax2.set_xticklabels(ax2.get_xticklabels())
+    if hasattr(bottomframe, 'chart_type'):
+        bottomframe.chart_type.get_tk_widget().destroy()
+    chart_type = FigureCanvasTkAgg(fig, bottomframe)
+    chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    bottomframe.chart_type = chart_type
 
 def show_graph5():
     crash_data = pd.read_csv('data.csv')
@@ -341,24 +376,23 @@ def show_graph5():
     crash_data['DAY'] = crash_data['ACCIDENT_DATE'].dt.day
     crash_data["HOUR"] = crash_data['ACCIDENT_TIME'].str[:2]
     crash_data["YEAR"] = crash_data['ACCIDENT_DATE'].dt.year
-    crash_data['ALCOHOL_RELATED'] = crash_data['ALCOHOL_RELATED'].str.lower()
     crash_data['On_Public_Holiday'] = crash_data['ACCIDENT_DATE'].dt.strftime('%d-%m').isin(vicholidays)
-    holiday_accident_counts = crash_data.groupby(['On_Public_Holiday', 'ACCIDENT_TYPE'])['ACCIDENT_TYPE'].count().unstack().fillna(0)
-    total_accidents_holiday = holiday_accident_counts.loc[True].sum()
-    total_accidents_non_holiday = holiday_accident_counts.loc[False].sum()
-    holiday_accident_percentage = (holiday_accident_counts.loc[True] / total_accidents_holiday) * 100
-    non_holiday_accident_percentage = (holiday_accident_counts.loc[False] / total_accidents_non_holiday) * 100
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.pie(holiday_accident_percentage, labels=holiday_accident_percentage.index, autopct='%1.1f%%', startangle=140)
-    plt.axis('equal')
-    plt.title('Accident Types on Public Holidays (All Data)')
-    plt.subplot(1, 2, 2)
-    plt.pie(non_holiday_accident_percentage, labels=non_holiday_accident_percentage.index, autopct='%1.1f%%', startangle=140)
-    plt.axis('equal')
-    plt.title('Accident Types on Non-Holidays (All Data)')
-    plt.tight_layout()
-    plt.show()
+    holiday_data = crash_data[crash_data['On_Public_Holiday']]
+    non_holiday_data = crash_data[~crash_data['On_Public_Holiday']]
+    holiday_counts = holiday_data['ACCIDENT_TYPE'].value_counts()
+    non_holiday_counts = non_holiday_data['ACCIDENT_TYPE'].value_counts()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    ax1.pie(holiday_counts, labels=holiday_counts.index, autopct='%1.1f%%', startangle=140)
+    ax1.axis('equal')
+    ax1.set_title('Accident Types on Public Holidays (All Data)')
+    ax2.pie(non_holiday_counts, labels=non_holiday_counts.index, autopct='%1.1f%%', startangle=140)
+    ax2.axis('equal')
+    ax2.set_title('Accident Types on Non-Holidays (All Data)')
+    if hasattr(bottomframe, 'chart_type'):
+        bottomframe.chart_type.get_tk_widget().destroy()
+    chart_type = FigureCanvasTkAgg(fig, bottomframe)
+    chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    bottomframe.chart_type = chart_type
 
 def show_graph52():
     crash_data = pd.read_csv('data.csv')
@@ -369,17 +403,22 @@ def show_graph52():
     crash_data["YEAR"] = crash_data['ACCIDENT_DATE'].dt.year
     crash_data['ALCOHOL_RELATED'] = crash_data['ALCOHOL_RELATED'].str.lower()
     crash_data['On_Public_Holiday'] = crash_data['ACCIDENT_DATE'].dt.strftime('%d-%m').isin(vicholidays)
-    holiday_alcohol_related = crash_data[(crash_data['On_Public_Holiday'] == True) & (crash_data['ALCOHOL_RELATED'] == 'yes')]
+    holiday_alcohol_related = crash_data[(crash_data['On_Public_Holiday']) & (crash_data['ALCOHOL_RELATED'] == 'yes')]
     holiday_alcohol_related['HOUR'] = holiday_alcohol_related['HOUR'].astype('int32')
-    holiday_alcohol_related = holiday_alcohol_related['HOUR'].value_counts().sort_index()
-    plt.figure(figsize=(10, 5))
-    plt.bar(holiday_alcohol_related.index, holiday_alcohol_related.values)
-    plt.title('Number of Alcohol-Related Accidents on Public Holidays 2013-2019')
-    plt.ylabel('Number of Accidents')
-    plt.xlabel('Hour of the day')
-    plt.xticks(rotation=0)
-    plt.tight_layout()
-    plt.show()
+    sorted_holiday_alcohol_related = holiday_alcohol_related['HOUR'].value_counts().sort_index()
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.bar(sorted_holiday_alcohol_related.index, sorted_holiday_alcohol_related.values)
+    ax.set_title('Number of Alcohol-Related Accidents on Public Holidays 2013-2019')
+    ax.set_ylabel('Number of Accidents')
+    ax.set_xlabel('Hour of the day')
+    ax.set_xticks(sorted_holiday_alcohol_related.index)
+    ax.set_xticklabels(sorted_holiday_alcohol_related.index, rotation=0)
+    fig.tight_layout()
+    if hasattr(bottomframe, 'chart_type'):
+        bottomframe.chart_type.get_tk_widget().destroy()
+    chart_type = FigureCanvasTkAgg(fig, bottomframe)
+    chart_type.get_tk_widget().grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    bottomframe.chart_type = chart_type
 
 root.mainloop()
 
